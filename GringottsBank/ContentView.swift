476f798fac20017734ee82b87eb485bda8a80588
Roy HAN <royhancn@outlook.com>
2023-10-15 14:53:05 +0800

    Initial Commit

---
+++ GringottsBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GringottsBank
+//
+//  Created by Roy on 2023/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GringottsBank/GringottsBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  GringottsBankApp.swift
+//  GringottsBank
+//
+//  Created by Roy on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct GringottsBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
