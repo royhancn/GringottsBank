8783b8467ab75c9d2370ebcde67497d031c654ac
Roy HAN <royhancn@outlook.com>
2023-10-22 15:44:39 +0800

    Add teasing popup just for fun

--- GringottsBank/SettingsView.swift
+++ GringottsBank/SettingsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct SettingsView: View {
     @ObservedObject var configDataModel = SharedStore.configDataModel
     @State private var monthlySalary:Double = 0
+    @State private var doAlert = false
+    let alertTitle: String = "Poor kid..."
     
     private static let formatter: NumberFormatter = {
             let formatter = NumberFormatter()
@@ -27,7 +29,24 @@ struct SettingsView: View {
         }.padding(.all)
         
         Button("Apply"){
-            self.configDataModel.updateConfig(monthlySalary: monthlySalary)
+            if monthlySalary < 100000 {
+                doAlert = true
+            }
         }.buttonStyle(.bordered)
+            .alert(
+                alertTitle,
+                isPresented: $doAlert
+            ) {
+                Button(role: .destructive) {
+                    self.configDataModel.updateConfig(monthlySalary: monthlySalary)
+                } label: {
+                    Text("Alright")
+                }
+                Button("You bad bad") {
+                    // Handle the retry action.
+                }
+            } message: {
+                Text("Are you sure you only earn such little amount of salary? So sad.")
+        }
     }
 }
